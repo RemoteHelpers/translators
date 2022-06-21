3f1d69b8551eb4b58bd598f9dbb37434385ec623
RemoteHelpers <dd@r-s.com>
2022-06-21 18:04:24 +0300

    fix selector, setup API, setAnchors

--- js/index.js
+++ js/index.js
@@ -22,9 +22,18 @@ const refs = {
   showMoreBtn: document.querySelector(".section-team .show-more-btn"),
   teamItems: document.querySelectorAll(".section-team .container > ul .item"),
   question: document.querySelectorAll(".section-question ul .item"),
-  selectInput: document.querySelector("form .select-lang-container"),
-  translateInput: document.querySelector("form .translated-lang-container"),
+  selectInput: document.querySelector('div[data-name="select-lang"]'),
+  translateInput: document.querySelector('div[data-name="translated-lang"]'),
+  anchorLinks: document.querySelectorAll('a[href="#section-main-form"]'),
+  selectPackage: document.getElementById("choose-package"),
+  heroForm: document.getElementById("hero-form"),
+  packageForm: document.getElementById("package-form"),
+  mainForm: document.getElementById("main-form"),
 };
+refs.anchorLinks.forEach(item => item.addEventListener("click", transferData));
+function transferData(e) {
+  refs.selectPackage.value = e.currentTarget.dataset.package;
+}
 refs.showMoreBtn.addEventListener("click", showMoreTeam);
 function showMoreTeam(e) {
   refs.teamItems.forEach(item => item.classList.add("visible"));
@@ -33,7 +42,7 @@ function showMoreTeam(e) {
 function hideBtn() {
   refs.showMoreBtn.style.display = "none";
 }
-
+console.log(refs.anchorLinks);
 refs.question.forEach(item => item.addEventListener("click", showAnswer));
 function showAnswer(e) {
   if (e.currentTarget.classList.contains("answered")) {
@@ -76,4 +85,49 @@ function openSelectMenu(e) {
   listRef.classList.add("visible");
   iconRef.classList.add("visible");
   menu[name] = true;
+}
+
+refs.heroForm.addEventListener("submit", onSubmitForm);
+refs.packageForm.addEventListener("submit", onSubmitForm);
+refs.mainForm.addEventListener("submit", onSubmitForm);
+function onSubmitForm(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  formData.set("global_company_name", "Remote Helpers");
+  formData.set("project_company", "trn-s.com");
+  switch (e.target.id) {
+    case "hero-form":
+      formData.set("client_name", "fast-lead");
+      formData.set("note", "-");
+      break;
+    case "package-form":
+      formData.set("note", "Personal Package");
+      break;
+    default:
+      const refLang = document.querySelectorAll(".js-target");
+      formData.set(
+        "note",
+        `Package: ${
+          !formData.get("package") ? "-" : formData.get("package")
+        }; Translation: ${refLang[0].firstElementChild.alt} to ${
+          refLang[1].firstElementChild.alt
+        }`,
+      );
+      formData.delete("package");
+      formData.delete("agreement");
+  }
+  addUserData(formData)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.log(error.message));
+  e.target.reset();
+}
+const url = "https://crm-s.com/api/v1/leads-public";
+async function addUserData(userData) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: userData,
+  });
+  return response.json();
 }
